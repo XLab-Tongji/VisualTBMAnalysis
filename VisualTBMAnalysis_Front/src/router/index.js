84bfd316e531f95e62831a18e08429f3a464c3f9
DiscreteL <1752962271@qq.com>
2022-01-06 15:08:48 +0800

    '1.6'

--- VisualTBMAnalysis_Front/src/router/index.js
+++ VisualTBMAnalysis_Front/src/router/index.js
@@ -35,22 +35,22 @@ const router = new Router({
 })
 
 //限制某些页面禁止未登录访问
-// let limitPagePath = ['/home', '/intro', '/data', '/analysis']
+let limitPagePath = ['/home', '/intro', '/data', '/analysis']
 
-// router.beforeEach((to, from, next) => {
-//     if (limitPagePath.includes(to.path)) {
-//         // 判断sessionStorage是否保存了用户信息
-//         let userStr = sessionStorage.getItem("user") || "{}"
-//         let user = JSON.parse(userStr)
-//         if (!user.id) {
-//             // 跳转到登录页面
-//             next({ path: "/login" })
-//         } else {
-//             next()
-//         }
-//     } else {
-//         next()
-//     }
-// })
+router.beforeEach((to, from, next) => {
+    if (limitPagePath.includes(to.path)) {
+        // 判断sessionStorage是否保存了用户信息
+        let userStr = sessionStorage.getItem("user") || "{}"
+        let user = JSON.parse(userStr)
+        if (!user.id) {
+            // 跳转到登录页面
+            next({ path: "/login" })
+        } else {
+            next()
+        }
+    } else {
+        next()
+    }
+})
 
 export default router
